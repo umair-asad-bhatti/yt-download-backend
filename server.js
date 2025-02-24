2a99fbe671f3d5cee0202085ae5d75a060613639
umair-asad-bhatti <umaircyber6@gmail.com>
2025-02-24 15:08:37 +0500

    added env port

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // For form data
 app.use(bodyParser.json()); // For JSON data (if your client sends JSON)
 app.use(router);
 app.use(express.static('public'));
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log('server listening on ', port);
 });
