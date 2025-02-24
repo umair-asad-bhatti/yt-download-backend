1530b6f771e141f842e9e1baaf16fe117ebdb9fe
umair-asad-bhatti <umaircyber6@gmail.com>
2025-02-24 15:22:54 +0500

    restrucured the app

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { createServer } = require('node:http');
+
 const ytdl = require('@distube/ytdl-core');
-const { Server } = require('socket.io');
+
 const app = express();
 app.use(cors());
-const server = createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-  },
-});
+
+
 const router = express.Router();
 app.use(bodyParser.urlencoded({ extended: true })); // For form data
 app.use(bodyParser.json()); // For JSON data (if your client sends JSON)
@@ -51,7 +47,7 @@ app.post('/checkInfo', async (req, res) => {
   }
 });
 const port = process.env.PORT || 3000;
-server.listen(port, () => {
+app.listen(port, () => {
   console.log('server listening on ', port);
 });
 module.exports = app;
